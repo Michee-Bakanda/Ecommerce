daeb409b4a1a250b43264c6d479809c65afdaef7
michee <micbakanda@gmail.com>
2022-09-08 03:25:56 +0200

    created the swiper

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "/image/slidepicture.png",
+        title:"Summer Sale",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR MEN ARRIVALS.",
+        bg:"lightgray"
+    },
+    {
+        id: 2,
+        img: "/image/slidepicture.png",
+        title:"Summer Sale",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR MEN ARRIVALS.",
+        bg:"lightgray"
+    },
+    {
+        id: 3,
+        img: "/image/slidepicture.png",
+        title:"Summer Sale",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR MEN ARRIVALS.",
+        bg:"lightgray"
+    },
+]
